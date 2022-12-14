612a0401e9afafe84bf053162c0b8742dfb95874
kary_ser <morskya.life@gmail.com>
2022-12-14 18:38:23 +0300

    made handlers for other buttons

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 'use strict';
 
+const headerBtn = document.querySelector('.header__btn');
+headerBtn.addEventListener('pointerover', (e) => {
+    e.target.style.cssText = `color: #3A79FF;
+    background-color: #D2E1FF;`
+});
+headerBtn.addEventListener('pointerout', (e) => {
+    e.target.style.cssText = `color: #697CA6;
+    background-color: #D6DAE5;` });
+
+
+const contentBtn = document.querySelector('.content__btn');
+contentBtn.addEventListener('pointerover', () => {
+    contentBtn.classList.toggle('animation-content__btn');
+});
+
+
 document.querySelectorAll('.costumers__img')
     .forEach((el) =>
         el.addEventListener('click', (e) => {
             e.target.style.backgroundColor = '#5982E7';
         }));
 
+
 document.querySelectorAll('form')
     .forEach(el => el.addEventListener('click', (event) => event.preventDefault()));
 
 const btnIllustrator = document.querySelector('.illustration__btn');
 const imgIllustrator = document.querySelector('.illustration__img');
-console.log(imgIllustrator);
-
-
 // btnIllustrator.addEventListener('pointerover', () => {
 //     btnIllustrator.classList.toggle('color');
 //     imgIllustrator.classList.toggle('active-img');
 // });
-
 // btnIllustrator.addEventListener('pointerout', () => {
 //     btnIllustrator.classList.toggle('color');
 //     imgIllustrator.classList.toggle('active-img');
 // });
-
 btnIllustrator.addEventListener("pointerdown", detectInputType);
 
 function detectInputType(event) {
@@ -41,6 +53,9 @@ function detectInputType(event) {
             imgIllustrator.classList.toggle('active-img');
             break;
         default:
-
     }
 }
+
+// проверяла на соотвествие h с макетом
+// const costumers = document.querySelector('.costumers');
+// console.log(costumers.offsetHeight);
