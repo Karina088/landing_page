f24a62afab43c6b16c5d8a8cc9223f3f5ef81132
kary_ser <morskya.life@gmail.com>
2022-12-22 16:52:22 +0300

    completed the drop down list

--- collapse.js
+++ collapse.js
@@ -1,20 +1,15 @@
 'use strict';
 
-const btnEl = document.querySelector('.faq-collapse__btn');
-// console.log(btnEl);
+const btnEl = document.getElementsByClassName('faq-collapse__btn');
 
-// for (let i = 0; i < btnEl.length; i++) {
-//     btnEl[i].addEventListener("click", function () {
-//         this.classList.toggle("active");
-//         let content = this.nextElementSibling;
-//         if (content.style.maxHeight) {
-//             content.style.maxHeight = null;
-//         } else {
-//             content.style.maxHeight = content.scrollHeight + "px";
-//         }
-//     });
-// }
-
-btnEl.addEventListener("click", () => {
-    btnEl.classList.toggle("faq-active");
-});
+for (let i = 0; i < btnEl.length; i++) {
+    btnEl[i].addEventListener("click", function () {
+        this.classList.toggle("faq-active");
+        let content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    })
+}
